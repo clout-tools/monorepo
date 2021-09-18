df9840bf4d68410a5f50abb6928ea5b424dc48a2
jshaw <jshaw3@amfam.com>
2021-09-17 21:40:42 -0500

    feat: generate sample package

---
+++ packages/sample-package/src/lib/sample-package.spec.ts
@@ -0,0 +1,7 @@
+import { samplePackage } from './sample-package';
+
+describe('samplePackage', () => {
+  it('should work', () => {
+    expect(samplePackage()).toEqual('sample-package');
+  });
+});
--- packages/sample/src/lib/sample.ts
+++
@@ -1,3 +0,0 @@
-export function sample(): string {
-  return 'Changing sample';
-}
