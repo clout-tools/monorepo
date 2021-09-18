f7591244d7b2c724725c1ec25d07c9e2510e053f
jshaw <jshaw3@amfam.com>
2021-09-17 21:47:19 -0500

    Revert "feat: generate sample package"
    This reverts commit df9840bf4d68410a5f50abb6928ea5b424dc48a2.

--- packages/sample-package/src/lib/sample-package.spec.ts
+++
@@ -1,7 +0,0 @@
-import { samplePackage } from './sample-package';
-
-describe('samplePackage', () => {
-  it('should work', () => {
-    expect(samplePackage()).toEqual('sample-package');
-  });
-});
--- packages/sample-package/src/lib/sample-package.ts
+++
@@ -1,3 +0,0 @@
-export function samplePackage(): string {
-  return 'sample-package';
-}
