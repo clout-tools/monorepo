b09a5097a7739bbdbf31cf63ae9cd0aaf32a3700
jshaw <jshaw3@amfam.com>
2021-09-17 22:44:20 -0500

    feat: minute-package

---
+++ packages/minute-package/src/lib/minute-package.spec.ts
@@ -0,0 +1,7 @@
+import { minutePackage } from './minute-package';
+
+describe('minutePackage', () => {
+  it('should work', () => {
+    expect(minutePackage()).toEqual('minute-package');
+  });
+});
